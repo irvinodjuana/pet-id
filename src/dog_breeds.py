38913975e0cafc54260ea0dc5cf90288d427f4b1
Irvino Djuana <irvino.djuana@gmail.com>
2019-12-29 19:25:14 -0800

    Refactored for code style

--- src/dog_breeds.py
+++ src/dog_breeds.py
@@ -11,11 +11,7 @@ from keras.callbacks import ModelCheckpoint
 from keras.layers.normalization import BatchNormalization
 from keras.models import Model, Sequential
 
-from PIL import ImageFile
-ImageFile.LOAD_TRUNCATED_IMAGES = True  # allow truncated imgs
-
 from sklearn.datasets import load_files
-
 import numpy as np
 from glob import glob
 from tqdm import tqdm
@@ -23,9 +19,13 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+from PIL import ImageFile
+
 # SSL setup to import pretrained models from keras.applications
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True  # allow truncated imgs
 
 
 # Load file/label strings from a directory of images
@@ -58,17 +58,32 @@ def paths_to_tensors(img_paths):
 def extract_Xception(file_paths):
     tensors = paths_to_tensors(file_paths).astype('float32')
     preprocessed_input = preprocess_input(tensors)
-    return Xception(weights='imagenet', include_top=False).predict(preprocessed_input, batch_size=32)
+    return Xception(weights='imagenet',
+                    include_top=False).predict(preprocessed_input,
+                                               batch_size=32)
 
 
 # Predict with model, labels, img
 def predict(dog_breeds, model, img_path):
     tensor = path_to_tensor(img_path)
-    feature = Xception(weights='imagenet', 
-        include_top=False).predict(preprocess_input(tensor))
+    feature = Xception(weights='imagenet',
+                       include_top=False).predict(preprocess_input(tensor))
+
+    # Get single prediction label (dog breed)
     predicted_vector = model.predict(feature)
-    prediction = dog_breeds[np.argmax(predicted_vector)].lower()
-    return prediction
+    prediction = np.argmax(predicted_vector)
+    predict_label = dog_breeds[prediction]
+
+    # Get top N breeds predicted by model with probabilities
+    NUM_VALUES = 5
+    class_prob = predicted_vector[0]
+    top_predict_values = sorted(range(len(class_prob)),
+                                key=lambda i: class_prob[i])[-1:-NUM_VALUES:-1]
+
+    for i in top_predict_values:
+        print(i, dog_breeds[i], class_prob[i], sep='\t')
+
+    return predict_label
 
 
 # Build second-level model
@@ -83,21 +98,24 @@ def input_branch(input_shape=None):
 
     return branch, branch_input
 
+
 # Get bottleneck features from pre-computed file
 def other_bottleneck_features(path):
     bottleneck_features = np.load(path)
-    train = bottleneck_features['train'] 
+    train = bottleneck_features['train']
     valid = bottleneck_features['valid']
     test = bottleneck_features['test']
-    return train,valid,test
+    return train, valid, test
 
 
 # a function that returns the prediction accuracy on test data
-def evaluate_model (model, model_name,tensors,targets):
-    predicted = [np.argmax(model.predict(np.expand_dims(feature, axis=0))) for feature in tensors]
-    test_accuracy = 100*np.sum(np.array(predicted)==np.argmax(targets, axis=1))/len(predicted)
-    
-    print (f'{model_name} accuracy on test data is {test_accuracy}%')
+def evaluate_model(model, model_name, tensors, targets):
+    predicted = [np.argmax(model.predict(np.expand_dims(feature, axis=0)))
+                 for feature in tensors]
+    test_accuracy = 100 * np.sum(np.array(predicted) == np.argmax(targets,
+                                                                  axis=1))
+    test_accuracy = test_accuracy / len(predicted)
+    print(f'{model_name} accuracy on test data is {test_accuracy}%')
 
 
 # Main
@@ -107,22 +125,25 @@ def main():
     checkpoint_dir = "../data/saved_models/"
 
     # Process data into files/labels
-    train_files, train_targets = load_dataset(images_dir + "train")
-    valid_files, valid_targets = load_dataset(images_dir + "valid")
-    test_files, test_targets = load_dataset(images_dir + "test")
+    # train_files, train_targets = load_dataset(images_dir + "train")
+    # valid_files, valid_targets = load_dataset(images_dir + "valid")
+    # test_files, test_targets = load_dataset(images_dir + "test")
 
     dog_breeds = [item.split("/")[-1].split(".")[-1]
                   for item in sorted(glob(images_dir + "train/*"))]
-    
-    # dog_breeds = [item[20:-1] for item in sorted(glob("dog/assets/images/train/*/"))]
 
-    # Obtain bottleneck features for training, validation, testing datasets
-    # bottleneck_file = "../data/assets/bottleneck_features/DogXceptionData.npz"
-    # train_Xception, valid_Xception, test_Xception = other_bottleneck_features(bottleneck_file)
+    # dog_breeds = [item[20:-1]
+    #               for item in sorted(glob("dog/assets/images/train/*/"))]
+
+    # Obtain bottleneck features for train/valid/test data
+    bottleneck_file = "../data/assets/bottleneck_features/ \
+                       DogXceptionData.npz"
+    train_Xception, valid_Xception, test_Xception = \
+        other_bottleneck_features(bottleneck_file)
     # train_Xception = extract_Xception(train_files)
     # valid_Xception = extract_Xception(valid_files)
     # test_Xception = extract_Xception(test_files)
-    
+
     # Build Xception model
     Xception_model = Sequential()
     Xception_model.add(GlobalAveragePooling2D(input_shape=(7, 7, 2048)))
@@ -130,13 +151,16 @@ def main():
     # Xception_model.summary()
 
     # Compile and train model
-    # Xception_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    # Xception_model.compile(loss='categorical_crossentropy',
+    #                        optimizer='rmsprop', metrics=['accuracy'])
     checkpoint_path = checkpoint_dir + "best_xception_model.hdf5"
-    # checkpointer = ModelCheckpoint(filepath=checkpoint_path, verbose=1 , save_best_only=True)
+    # checkpointer = ModelCheckpoint(filepath=checkpoint_path,
+    #                                verbose=1 , save_best_only=True)
 
     # Xception_history = Xception_model.fit(train_Xception, train_targets,
     #               validation_data = (valid_Xception , valid_targets),
-    #               epochs=25, batch_size=20, callbacks=[checkpointer], verbose=1)
+    #               epochs=25, batch_size=20, callbacks=[checkpointer],
+    #               verbose=1)
 
     # Load model weights
     Xception_model.load_weights(checkpoint_path)
@@ -144,10 +168,12 @@ def main():
     # Evaluate model against testing dataset
     # evaluate_model(Xception_model, "Xception" , test_Xception, test_targets)
 
-    img_path = "../data/assets/images/test/113.Old_english_sheepdog/Old_english_sheepdog_07376.jpg"
-    print(predict(dog_breeds, Xception_model, img_path))
+    img_path = "/Users/irvinodjuana/Desktop/rosie.png"
+    print("\n")
+    prediction = predict(dog_breeds, Xception_model, img_path)
+    print("\n" + prediction)
     plt.imshow(mpimg.imread(img_path))
-
+    print("\n")
 
 
 if __name__ == "__main__":

173e2dc802886e05a0a161d52ca4108fa93c6a82
Irvino Djuana <irvino.djuana@gmail.com>
2020-01-05 18:32:58 -0800

    Added graph into frontend react page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,92 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
-import Image from 'react-bootstrap/Image'
-// import Figure from 'react-bootstrap/Figure'
-// import InputGroup from 'react-bootstrap/InputGroup'
-// import FormControl from 'react-bootstrap/FormControl'
+import Image from 'react-bootstrap/Image';
+import Alert from 'react-bootstrap/Alert';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import axios from 'axios'
+import Chart from 'react-apexcharts';
+import axios from 'axios';
 
-// class ImageFigure extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       file: '',
-//       imagePreviewUrl: '',
-//       uploaded: false
-//     };
-//     this.handleImageChange = this.handleImageChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
 
-//   // Handle user submitting image
-//   handleSubmit(e) {
-//     e.preventDefault();
-//     // TODO: Handle user submitting file
-//   }
+function Graph(props) {
+  let labels = [];
+  let probabilities = []
+  props.predictions.forEach(item => {
+    labels.push(item.label.split("_").map(w => w[0].toUpperCase() + w.substr(1)).join(" "))
+    probabilities.push(item.probability)
+  });
+  console.log(labels);
+  console.log(probabilities);
 
-//   // Handle new image
-//   handleImageChange(e) {
-//     e.preventDefault();
+  let chartOptions = {
+      chart: {
+        type: 'bar',
+        height: 350
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      xaxis: {
+        categories: labels,
+      }
+  };
 
-//     let reader = new FileReader();
-//     let file = e.target.files[0];
+  let chartSeries = [
+    {data: probabilities}
+  ];
 
-//     reader.onloadend = () => {
-//       this.setState({
-//         file: file,
-//         imagePreviewUrl: reader.result,
-//         uploaded: true
-//       });
-//     }
-
-//     reader.readAsDataURL(file);
-//   }
-
-//   // Render in react
-//   render() {
-//     let imagePreviewUrl = "https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg";
-//     let fileSelectText = this.state.uploaded ? this.state.file.name : "Choose file";
-//     if (this.state.imagePreviewUrl !== '') {
-//       imagePreviewUrl = this.state.imagePreviewUrl;
-//     }
-
-//     return (
-//         <React.Fragment>
-//           <Image
-//             className = "mt-5"
-//             width={200}
-//             height={200}
-//             alt="image-upload" 
-//             src={imagePreviewUrl} 
-//             thumbnail rounded/>
-//           <div className="input-group mt-2 mb-2">
-//           <div className="custom-file">
-//             <input
-//               type="file"
-//               className="custom-file-input"
-//               id="inputGroupFile01"
-//               aria-describedby="inputGroupFileAddon01"
-//               accept="image/*"
-//               onChange={this.handleImageChange}
-//             />
-//             <label className="custom-file-label" htmlFor="inputGroupFile01">
-//               {fileSelectText}
-//             </label>
-//           </div>
-//         </div>
-//       </React.Fragment>
-//     );
-//   }
-
-// }
+  return (
+    <div>
+      <Chart options={chartOptions} series={chartSeries} type="bar"/>
+    </div>);
+}
 
 class App extends Component {
 
@@ -95,7 +58,10 @@ class App extends Component {
     this.state = {
       file: '',
       imagePreviewUrl: '',
-      uploaded: false
+      imageSelected: false,
+      predictions: null,
+      dog_found: false,
+      isLoading: false,
     };
     this.handleImageChange = this.handleImageChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -109,25 +75,36 @@ class App extends Component {
       return
     }
 
+    // Prediction endpoint
     let url = 'http://localhost:4321/predict'
 
     let formData = new FormData();
     formData.append('image', this.state.file);
+    this.setState({
+      isLoading: true
+    })
     axios.post(url, formData, {
       headers: {
         'content-type': 'multipart/form-data'
       }
     }).then((res) => {
-      console.log(res.data);
+      // console.log(res.data);
+      this.setState({
+        predictions: res.data.predictions,
+        dogFound: res.data.dog_found,
+        isLoading: false
+      });
     }).catch((err) => {
-      console.log(err)
+      console.log(err);
+      this.setState({
+        isLoading: false
+      });
     });
   }
 
   // Handle new image
   handleImageChange(e) {
     e.preventDefault();
-
     let reader = new FileReader();
     let file = e.target.files[0];
 
@@ -135,21 +112,50 @@ class App extends Component {
       this.setState({
         file: file,
         imagePreviewUrl: reader.result,
-        uploaded: true
+        imageSelected: true
       });
     }
-
     reader.readAsDataURL(file);
+  }
+
+  // Handle chart
+  chartHandler() {
+    if (this.state.isLoading) {
+      return ("loading...");
+    } else if (this.state.predictions) {
+      return (<Graph predictions={this.state.predictions}/>);
+    } else {
+      return null;
+    }
+  }
+
+  // Handle alert
+  alertHandler() {
+    let variant;
+    let text;
+    if (this.state.isLoading) {
+      variant = "warning";
+      text = "Loading...";
+    } else if (this.state.predictions) {
+      variant = this.state.dogFound ? "success" : "danger";
+      text = this.state.dogFound ? "Dog found!" : "Dog not found :(";
+    } else {
+      return null;
+    }
+
+    return (<Alert variant={variant}>{text}</Alert>);
   }
 
   // React render
   render() {
-    let isLoading = false;
-    let imagePreviewUrl = "https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg";
-    let fileSelectText = this.state.uploaded ? this.state.file.name : "Choose file";
+    let imagePreviewUrl = "https://glowvarietyshow.com/wp-content/uploads/2017/03/placeholder-image.jpg";
+    let fileSelectText = this.state.imageSelected ? this.state.file.name : "Choose file";
     if (this.state.imagePreviewUrl !== '') {
       imagePreviewUrl = this.state.imagePreviewUrl;
     }
+
+    let chartElement = this.chartHandler();
+    let alertElement = this.alertHandler();
   
     return (
       <Container>
@@ -157,17 +163,16 @@ class App extends Component {
           <h1 className="title">Dog Breed Classifier</h1>
         </div>
         <div className="content">
-        <Form>
           <Row>
-            <Col>
+            <Col sm={4} md={4}> 
               <Image
-              className = "mt-5"
-              width={200}
-              height={200}
-              alt="image-upload" 
-              src={imagePreviewUrl} 
-              thumbnail rounded/>
-              <div className="input-group mt-2 mb-2">
+                className = "mt-2"
+                width={200}
+                height={200}
+                alt="image-upload" 
+                src={imagePreviewUrl} 
+                thumbnail rounded/>
+              <div className="input-group mt-3 mb-3">
                 <div className="custom-file">
                   <input
                     type="file"
@@ -183,31 +188,24 @@ class App extends Component {
                 </div>
               </div>
             </Col>
-            <Col>
-              Graph goes here
+            <Col sm={8} md={8}>
+              {chartElement}
             </Col>
           </Row>
           <Row>
-            <Col>
+            <Col sm={4} md={4}>
               <Button
                 block
-                variant="success"
-                disabled={isLoading}
-                onClick={!isLoading ? this.handleSubmit : null}>
-                { isLoading ? 'Making prediction' : 'Predict' }
+                variant="outline-success"
+                disabled={this.state.isLoading}
+                onClick={!this.state.isLoading ? this.handleSubmit : null}>
+                {this.state.isLoading ? 'Making prediction' : 'Predict' }
               </Button>
             </Col>
-            <Col>
-              <Button
-                block
-                variant="danger"
-                disabled={isLoading}
-                onClick={this.handleCancelClick}>
-                Reset prediction
-              </Button>
+            <Col sm={8} md={8}>
+             {alertElement}
             </Col>
           </Row>
-          </Form>
         </div>
       </Container>
     );
